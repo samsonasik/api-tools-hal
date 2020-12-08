f3438eb3eac76da2ca945c02391d4806d73832c3
andyo <javabudd@gmail.com>
2020-12-08 13:56:14 -0700

    Add PHP 8.0 support
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Increase minimum PHP version to 7.4. Allow PHP ^8.0
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Update php 8 constraint. Update several laminas packages to latest versions support PHP 7.4 & 8
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Require ext-json in dev
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Add return types and \ to function calls
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Add void return type to setUp method
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Fix typo
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Add void return types
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Add void return types and \ to function call
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Fix grammar error
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Add void return types and organize methods
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Add void return types
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Add return types
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Add return types
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Update assertInternalType methods
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Revert method move
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Update assertContains calls
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Fix assertion
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Switch to PHP 7.3 for min
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Add lowest/latest deps testing
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    add --ignore-platform-reqs to COMPOSER_ARGS env
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Update matrixes
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    revert arg removal
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Use explicit 8.0 image
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Update ruleset path
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    revert version changes
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    revert constraints
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    revert constraints
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    require laminas module-manager
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Implement ConfigProviderInterface and BootstrapListenerInterface
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Update phpunit.xml.dist schema
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Add phpspec/prophecy-phpunit. Update laminas/laminas-view to ^2.11.4
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Use ProphecyTrait. Add return types. Use self when calling static methods
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Add phpunit result cache to gitignore
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    require laminas/laminas-hydrator ^3.2.0 || ^4.0.1
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Move modulemanager to require-dev
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Add FactoryInterface implementations
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Update factory assertions
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    add ext-json to require
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Add return types
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Add return types and cleanup code
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Add return types and FQN calls
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    add instance assertion
    
    Signed-off-by: andyo <javabudd@gmail.com>
    
    Fix extraction expectation
    
    Address deprecations and use self for assertion methods
    
    Signed-off-by: andyo <javabudd@gmail.com>

--- test/Extractor/EntityExtractorTest.php
+++ test/Extractor/EntityExtractorTest.php
@@ -14,23 +14,26 @@ use Laminas\Hydrator\ObjectProperty;
 use Laminas\Hydrator\ObjectPropertyHydrator;
 use LaminasTest\ApiTools\Hal\Plugin\TestAsset;
 use PHPUnit\Framework\TestCase;
+use Prophecy\PhpUnit\ProphecyTrait;
 
 /**
  * @subpackage UnitTest
  */
 class EntityExtractorTest extends TestCase
 {
+    use ProphecyTrait;
+
     /** @var string */
     private $hydratorClass;
 
-    public function setUp()
+    public function setUp(): void
     {
-        $this->hydratorClass = class_exists(ObjectPropertyHydrator::class)
+        $this->hydratorClass = \class_exists(ObjectPropertyHydrator::class)
             ? ObjectPropertyHydrator::class
             : ObjectProperty::class;
     }
 
-    public function testExtractGivenEntityWithAssociateHydratorShouldExtractData()
+    public function testExtractGivenEntityWithAssociateHydratorShouldExtractData(): void
     {
         $hydrator = new $this->hydratorClass();
 
@@ -40,10 +43,10 @@ class EntityExtractorTest extends TestCase
 
         $extractor = new EntityExtractor($entityHydratorManager->reveal());
 
-        $this->assertSame($extractor->extract($entity), $hydrator->extract($entity));
+        self::assertSame($extractor->extract($entity), $hydrator->extract($entity));
     }
 
-    public function testExtractGivenEntityWithoutAssociateHydratorShouldExtractPublicProperties()
+    public function testExtractGivenEntityWithoutAssociateHydratorShouldExtractPublicProperties(): void
     {
         $entity = new TestAsset\Entity('foo', 'Foo Bar');
         $entityHydratorManager = $this->prophesize(EntityHydratorManager::class);
@@ -52,12 +55,12 @@ class EntityExtractorTest extends TestCase
         $extractor = new EntityExtractor($entityHydratorManager->reveal());
         $data = $extractor->extract($entity);
 
-        $this->assertArrayHasKey('id', $data);
-        $this->assertArrayHasKey('name', $data);
-        $this->assertArrayNotHasKey('doNotExportMe', $data);
+        self::assertArrayHasKey('id', $data);
+        self::assertArrayHasKey('name', $data);
+        self::assertArrayNotHasKey('doNotExportMe', $data);
     }
 
-    public function testExtractTwiceGivenSameEntityShouldProcessExtractionOnceAndReturnSameData()
+    public function testExtractTwiceGivenSameEntityShouldProcessExtractionOnceAndReturnSameData(): void
     {
         $entity = new TestAsset\Entity('foo', 'Foo Bar');
         $entityHydratorManager = $this->prophesize(EntityHydratorManager::class);
@@ -68,6 +71,6 @@ class EntityExtractorTest extends TestCase
         $data1 = $extractor->extract($entity);
         $data2 = $extractor->extract($entity);
 
-        $this->assertSame($data1, $data2);
+        self::assertSame($data1, $data2);
     }
 }
